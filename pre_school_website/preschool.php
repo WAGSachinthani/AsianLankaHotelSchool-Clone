110cfe51a452fa2f2956b6710550245e3d63bbb3
W.A.G.Sachinthani <158131520+WAGSachinthani@users.noreply.github.com>
2024-04-23 15:06:37 +0530

    Add files via upload

---
+++ pre_school_website/preschool.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="preschool.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pre-School Teacher Training Course</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo-up">
+                <img src="logo.png" alt="Logo">
+            </div>
+
+            <ul class="dropdown">
+                    <div class="dropdown">
+                        <a href="#">English</a>
+                        <a href="#">Sinhala</a>
+                        <a href="#">Tamil</a>
+                    </div>
+            </ul>
+            <ul class="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#vocational">Vocational</a></li>
+                <li><a href="#packages">Packages</a></li>
+                <li><a href="#programs">Our Programs</a></li>
+                <li><a href="#application">Application</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <h1>Child Care Center Operations N.V.Q. Level – 4</h1>
+    </header>
+
+    <section id="header">
+        <h2>Pre-School Teacher & Early Childhood Development (N.V.Q Level 4)</h2>
+        <?php
+       
+        ?>
+    </section>
+
+    <section id="fee">
+        <h3>Course Fee : Rs 99,900</h3>
+
+        <?php
+       
+        ?>
+    </section>
+
+    <section id="duration">
+        <h3>Course Duration (Months) : 06</h3>
+
+        <?php
+       
+        ?>
+    </section>
+
+    <section id="content">
+        <h3>Course Content</h3>
+            <ul class="topics">
+                <li>Maintain Housekeeping Standards</a></li>
+                <li>Arrange Physical Environment</a></li>
+                <li>Maintain Health, Safety & Effective Environment</a></li>
+                <li>Provide Food & Beverages</a></li>
+                <li>Provide Physical Care</a></li>
+                <li>Foster Holistic Development of Children</a></li>
+                <li>Administer Medications & Provide First Aids</a></li>
+                <li>Carry out Child Care Centre Administration Activities</a></li>
+                <li>Maintain Professionalism</a></li>
+                <li>Care of a Child With Special Needs</a></li>
+            </ul>
+    </section>
+
+    <section id="button">
+        <button>Register Now</button>
+    </section>
+
+    <section>
+        <div class="logo-footer">
+            <img src="logo.png" alt="Logo">
+        </div>
+    </section>
+    <br>
+
+ 
+    <footer>
+        <div class="social-icons">
+                <a href="#" class="icon"><img src="social\fb.webp" alt="Facebook"></a>
+                <a href="#" class="icon"><img src="social\linkdn.png" alt="Twitter"></a>
+                <a href="#" class="icon"><img src="social\inster.webp" alt="Instagram"></a>
+                <a href="#" class="icon"><img src="social\tiktok.png" alt="LinkedIn"></a>
+                <a href="#" class="icon"><img src="social\yout.webp" alt="LinkedIn"></a>
+                <a href="#" class="icon"><img src="social\whatsapp.jpg" alt="LinkedIn"></a>
+        </div>
+        <p>© 2024 Asia Lanka Hotel School</p>
+    </footer>
+</body>
+</html>
